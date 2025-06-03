133ea8b62dde17aba3cfe677030061cf0129d856
neela <nchikhali0001@mymail.lausd.net>
2025-06-03 10:45:22 -0700

    updated tucker.java

--- src/main/java/com/ceela/tucker.java
+++ src/main/java/com/ceela/tucker.java
@@ -10,37 +10,69 @@ public class tucker{
         // bird.setMove ("F",10,50);
 
         int e = NoteCreator.findNote("E");
+        int a = NoteCreator.findNote("A");
         int d = NoteCreator.findNote("D");
-        int c = NoteCreator.findNote("C");
         int g = NoteCreator.findNote("G");
+        int f = NoteCreator.findNote("F#/Gb");
+        int b = NoteCreator.findNote("B");
+        int c = NoteCreator.findNote("C");
 
 
-        bird.playNote(e, 1);
+        bird.playNote(d, 1);
+        bird.pause(0.25);
+         bird.playNote(d, 1);
+        bird.pause(0.25);
+         bird.playNote(e, 1);
         bird.pause(0.25);
         bird.playNote(d, 1);
         bird.pause(0.25);
-        bird.playNote(c, 1);
+         bird.playNote(g, 1);
         bird.pause(0.25);
-        bird.playNote(d, 1);
+         bird.playNote(f, 2);
         bird.pause(0.25);
-        bird.playNote(e, 1);
+
+         bird.playNote(d, 1);
         bird.pause(0.25);
-        bird.playNote(e, 1);
+         bird.playNote(d, 1);
         bird.pause(0.25);
-        bird.playNote(e, 1);
+         bird.playNote(e, 1);
+        bird.pause(0.25);
+         bird.playNote(d, 1);
+        bird.pause(0.25);
+         bird.playNote(a, 1);
+        bird.pause(0.25);
+        bird.playNote(g, 2);
+        bird.pause(0.25);
+
+       bird.playNote(d, 1);
+        bird.pause(0.25);
+         bird.playNote(d, 1);
+        bird.pause(0.25);
+         bird.playNote(d, 1);
+        bird.pause(0.25);
+        bird.playNote(b, 1);
+        bird.pause(0.25);
+         bird.playNote(g, 1);
+        bird.pause(0.25);
+         bird.playNote(f, 1);
         bird.pause(0.5);
+          bird.playNote(e, 2);
+        bird.pause(0.25);
 
-        bird.playNote(d, 1);
+         bird.playNote(c, 1);
         bird.pause(0.25);
-        bird.playNote(d, 1);
+         bird.playNote(c, 1);
         bird.pause(0.25);
-        bird.playNote(d, 1);
-        bird.pause(0.5);
-        bird.playNote(e, 1);
+         bird.playNote(b, 1);
         bird.pause(0.25);
         bird.playNote(g, 1);
-        bird.pause(0.5);
+        bird.pause(0.25);
+         bird.playNote(a, 1);
+        bird.pause(0.25);
+         bird.playNote(g, 2);
+        bird.pause(0.25);
 
+      
 
         bird.stop();
         bird.disconnect();
