01552aedeb0f1c0aad63542a6ff7f8ee2181f5e7
neela <nchikhali0001@mymail.lausd.net>
2025-06-06 09:44:27 -0700

    finihse dsome

--- src/main/java/com/ceela/tucker.java
+++ src/main/java/com/ceela/tucker.java
@@ -2,12 +2,13 @@ package com.ceela;
 
 import com.birdbrain.Finch;
 
+//
 public class tucker{
 
     public static void main (String[] args){
         Finch bird = new Finch();
         
-        // bird.setMove ("F",10,50);
+        // Finds the note needed from the midi to musical notes txt
 
         int e = NoteCreator.findNote("E");
         int a = NoteCreator.findNote("A");
@@ -17,7 +18,7 @@ public class tucker{
         int b = NoteCreator.findNote("B");
         int c = NoteCreator.findNote("C");
 
-
+      // plays the song happy birthday
         bird.playNote(d, 1);
         bird.pause(0.25);
          bird.playNote(d, 1);
@@ -71,8 +72,15 @@ public class tucker{
         bird.pause(0.25);
          bird.playNote(g, 2);
         bird.pause(0.25);
-
-      
+        // displays happy birthday and then makes bird dance 
+      bird.print("HAPPY BIRTHDAY");
+      bird.setBeak(0,100,0);
+      bird.setMove("F",10,75);
+      bird.setMove("B",10,75);
+      bird.setTurn("R",360,100);
+       bird.setTurn("L",360,100);
+        bird.setTurn("R",360,100);
+            bird.setTurn("L",360,100);
 
         bird.stop();
         bird.disconnect();
